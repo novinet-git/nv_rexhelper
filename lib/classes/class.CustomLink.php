bcfb0e474ace8fdd67b48ac552101aff7068cda0
novinet-markusd <markus.dick@novinet.de>
2020-01-20 14:34:03 +0100

    Create class.CustomLink.php

---
+++ lib/classes/class.CustomLink.php
@@ -0,0 +1,41 @@
+<?php 
+namespace nvRexHelper;
+
+class nvCustomLink {
+	private $href;
+	private $target;
+
+	function __construct($sPath) {
+		if(!$sPath) {
+			$this->href = "";
+			$this->target = "";
+			return;
+		} 
+
+		if(filter_var($sPath, FILTER_VALIDATE_URL)) {
+			// externe url
+			$this->href = $sPath;
+			$this->target = "_blank";
+		} else {
+			if(filter_var($sPath, FILTER_VALIDATE_INT)) {
+				// interne url
+				$this->href = rex_getUrl($sPath);
+				$this->target = "_self";
+			} else {
+				// media
+				$this->href = MEDIA . $sPath;
+				$this->target = "_blank";
+			}
+		}
+
+
+	}
+
+	public function getHref() {
+		return $this->href ? $this->href : ""; 
+	}
+
+	public function getTarget() {
+		return $this->target ? $this->target : "";
+	}
+} 

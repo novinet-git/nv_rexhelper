6bbf33f77fc1d143ccf17d9ea4f3be0925bd914a
novinet-markusd <markus.dick@novinet.de>
2020-02-17 17:45:05 +0100

    Update constants.php

--- lib/constants.php
+++ lib/constants.php
@@ -1,5 +1,5 @@
 <?php
-define("FE", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
-define("ASSETS", FE . "/theme/public/assets/frontend/");
-define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
-define("MEDIA", FE . "/media/");
+const FE = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+const ASSETS = FE . "/theme/public/assets/frontend/";
+const ROOT = $_SERVER["DOCUMENT_ROOT"];
+const MEDIA = FE . "/media/";

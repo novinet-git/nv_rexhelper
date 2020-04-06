424870f99d54e2cdbf0a66b3125ac1b768e82ac3
novinet-markusd <markus.dick@novinet.de>
2020-04-06 15:43:44 +0200

    add PictureTag::generateLazy

--- lib/classes/class.PictureTag.php
+++ lib/classes/class.PictureTag.php
@@ -33,6 +33,41 @@ class PictureTag {
     }
 
     /**
+     * generate a picture tag for lazy load https://github.com/verlok/lazyload
+     * 
+     * @param string $media
+     * @param array $attributes e.g. ["class" => "nv-image"]
+     * 
+     * @return string
+     */
+
+    public static function generateLazy($media, $attributes=[]) {
+
+        if (!isset($attributes["class"])) {
+            $attributes["class"] = "lazy";
+        } else {
+            $attributes["class"] .= " lazy";
+        }
+
+        $attr = "";
+        foreach($attributes as $key => $value) {
+            $attr .= $key .'="'. $value .'" '; 
+        }
+
+        $return = '<picture>';
+
+        $srcSM = MEDIA . 'max_width_sm/' . $media;
+        $srcLG = MEDIA . 'max_width_lg/' . $media;
+        $src = MEDIA . $media; 
+       
+        $return .= '<source media="(max-width: 575px)" srcset="'.$srcSM.'">';
+        $return .= '<source media="(max-width: 991px)" srcset="'.$srcLG.'">';
+        $return .= '<img '.$attr.' data-src="'.$src.'">';
+
+        return $return . '</picture>';
+    }
+
+    /**
      * generate a background tag for a given media
      * 
      * @param string $media

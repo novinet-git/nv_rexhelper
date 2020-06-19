1c6de4c093903f2d3f0ae04e38ea33a0a9862a42
novinet-markusd <markus.dick@novinet.de>
2020-06-19 12:33:03 +0200

    change ROOT
    from $_SERVER["DOCUMENT_ROOT" to rex_path::base()

--- lib/constants.php
+++ lib/constants.php
@@ -1,5 +1,5 @@
 <?php
 define("FE", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 define("ASSETS", FE . "/theme/public/assets/frontend/");
-define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
+define("ROOT", rex_path::base());
 define("MEDIA", FE . "/media/");

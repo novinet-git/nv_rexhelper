f87443d08561343e653cfe2206d0e0a28cc978f5
novinet-markusd <markus.dick@novinet.de>
2020-10-05 15:23:05 +0200

    add subtree

--- lib/classes/class.StructureTree.php
+++ lib/classes/class.StructureTree.php
@@ -12,29 +12,62 @@ class StructureTree {
 
     public static function factory(int $root_id): StructureTree {
         if (!array_key_exists("$root_id", self::$cache)) {
-            self::$cache["$root_id"] = new self($root_id);
+            self::$cache["$root_id"] = new self($root_id, [], "");
         } 
         return self::$cache["$root_id"];
     }
 
     private $tree = [];
-    private $root_category = null;
-    private $root_id = null;
     private $article_id_active = 0;
     private $active_path = "";
 
-    public function __construct(int $root_id) {
-        $this->root_id = $root_id;
+    public function __construct(int $root_id, array $tree, string $active_path) {
         $this->article_id_active = \rex_article::getCurrentId();
-        if (!$this->root_category = \rex_category::get($root_id)) throw new \rex_exception("given id $root_id is not a category");
-        $this->tree["$root_id"] = $this->get_structure_tree_rec($this->root_category, 0);
-        $this->set_active_structure($this->tree, $this->active_path);
+
+        if (!$tree) {
+            if (!$root_category = \rex_category::get($root_id)) throw new \rex_exception("given id $root_id is not a category");
+            $this->tree["$root_id"] = $this->get_structure_tree_rec($root_category, 0);
+            $this->set_active_structure($this->tree, $this->active_path);
+        } else if ($tree && $active_path) {
+            $this->tree = $tree;
+            $this->active_path = $active_path;
+        }
+
     }
 
-    public function get_tree(): array {
+    /**
+    ** get the tree array
+    */
+
+    public function get_array(): array  {
         return $this->tree;
     }
 
+    /**
+    ** get a part of the tree, we also store that part in the chache
+    */
+
+    public function get_sub_tree(int $root_id): StructureTree {
+        if (!\array_key_exists("$root_id", self::$cache)) {
+            $tree = $this->get_sub_tree_rec($this->get_tree(), $root_id); 
+            if (!$tree) throw new \rex_exeption("couldn't find subtree, please check your code");
+            self::$cache["$root_id"] = new self($root_id, $tree, $this->active_path);
+        }
+
+        return self::$cache["$root_id"];
+    }
+
+    private function get_sub_tree_rec(array $item, int $search): array {
+        if (\array_key_exists($search, $item)) return ["$search" => $item[$search]];
+        
+        foreach($item as $child) {
+            if ($child["children"]) {
+                return $this->get_sub_tree_rec($child["children"], $search);
+            }
+        }
+
+        return [];
+    }
 
     /**
     ** running through the tree recursivly
@@ -56,6 +89,7 @@ class StructureTree {
 
             $articles["$id"] = [
                 "active" => $active,
+                "depth" => $depth,
                 "article" => $child,
             ];
         }
@@ -68,6 +102,7 @@ class StructureTree {
         return [
             "active" => false,
             "depth" => $depth,
+            "category" => $category,
             "articles" => $articles,
             "children" => $children,
         ];
@@ -78,12 +113,7 @@ class StructureTree {
     */
 
     private function set_active_structure(array &$tree, string $active_path) {
-        dump($active_path);
-
-        $iterator = explode("|", $active_path);
-        $iterator = array_filter($iterator, function($item) {
-            return $item != "";
-        });
+        $iterator = array_filter(explode("|", $active_path), function($item) { return $item != ""; });
       
         while($iterator) {
             $key = array_shift($iterator);     
